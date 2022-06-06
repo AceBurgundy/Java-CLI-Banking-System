e8fdbf3ff01db47d1de93714ef1055fb294d90cc
AceBurgundy <samadriansabalo99@gmail.com>
2022-06-06 19:27:27 +0800

    Added create account method

--- Machine/Tools.java
+++ Machine/Tools.java
@@ -1,6 +1,19 @@
 package Machine;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Tools {
+
+    private final static Scanner input = new Scanner(System.in);
+    private static PreparedStatement inSql = null;
+    private static Connection connect = null;
+
     public static void showMenu() {
         System.out.println("\n\tMENU");
         System.out.println("\t*******************************");
@@ -14,4 +27,70 @@ public class Tools {
         System.out.println("\t*******************************");
 
     }
+
+    public static void createAccount() {
+
+        Helpers.clear();
+        showMenu();
+        System.out.println("\n\t*******************************");
+        System.out.println("\t**      Create Account      ***");
+        System.out.println("\t*******************************");
+        System.out.println("\n\tEnter Details: ");
+        System.out.println("\t*******************************");
+        System.out.println("\tFirst Name: ");
+        String dbfirstName = Helpers.inputString();
+        System.out.println("\t*******************************");
+        System.out.println("\tLast Name: ");
+        String dblastName = Helpers.inputString();
+        System.out.println("\t*******************************");
+        System.out.println("\tPassword: ");
+        String dbpassword = Helpers.inputString();
+        System.out.println("\t*******************************");
+        System.out.println("\tInitial Deposit: ");
+        int dbBalance = Helpers.inputInt(2000000000, 0);
+
+        try {
+            String uri = "jdbc:sqlite:user.db";
+            connect = DriverManager.getConnection(uri);
+            int accountNumber = Helpers.generateAccountNumber();
+
+            inSql = connect.prepareStatement(
+                    "INSERT INTO accounts(account_number, first_name, last_name, password, balance) VALUES(?,?,?,?,?)");
+            inSql.setInt(1, accountNumber);
+            inSql.setString(2, dbfirstName);
+            inSql.setString(3, dblastName);
+            inSql.setString(4, Helpers.encrypt(dbpassword));
+            inSql.setInt(5, dbBalance);
+            inSql.executeUpdate();
+
+            Helpers.clear();
+            System.out.println("\n\n\t*******************************");
+            System.out.println("\t** Acount Saved Sucessfully ***");
+            System.out.println("\t*******************************");
+            System.out.println("\n\tYour account details are; ");
+            System.out.println("\tAccount Number: " + accountNumber);
+            System.out.println("\tFirst Name: " + dbfirstName);
+            System.out.println("\tLast Name: " + dblastName);
+            System.out.println("\tInitial Deposit: " + dbBalance);
+            System.out.println("\n\tFor safety reason/s your password will not be shown.");
+            System.out.println(
+                    "\n\tDont forget to memorize your accout number as you will be using it to access your account.\n");
+
+            Helpers.prompt();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("\tAccount Not Saved\n");
+        } finally {
+            try {
+                if (connect != null) {
+                    connect.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
 }
