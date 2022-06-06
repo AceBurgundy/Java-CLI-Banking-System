46fc13bd41a50c3b7023bd2e0e6d9628391bb600
AceBurgundy <samadriansabalo99@gmail.com>
2022-06-06 19:16:38 +0800

    Added showMenu() to show menu of options to the user.

--- Machine/Tools.java
+++ Machine/Tools.java
@@ -1,4 +1,17 @@
 package Machine;
 
 public class Tools {
+    public static void showMenu() {
+        System.out.println("\n\tMENU");
+        System.out.println("\t*******************************");
+        System.out.println("\t*                             *");
+        System.out.println("\t*       1. Create Account     *");
+        System.out.println("\t*       2. Check Balance      *");
+        System.out.println("\t*       3. Deposit            *");
+        System.out.println("\t*       4. Withdraw           *");
+        System.out.println("\t*       5. Exit               *");
+        System.out.println("\t*                             *");
+        System.out.println("\t*******************************");
+
+    }
 }
