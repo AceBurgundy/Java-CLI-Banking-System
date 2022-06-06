f277c60de0701380b70285d74fcbc0c90e087c94
AceBurgundy <samadriansabalo99@gmail.com>
2022-06-06 19:23:40 +0800

    Added console object and input object with its imports

--- Machine/Helpers.java
+++ Machine/Helpers.java
@@ -1,4 +1,9 @@
 package Machine;
 
+import java.util.Scanner;
+import java.io.Console;
+
 public class Helpers {
+   private final static Console console = System.console();
+   static Scanner input = new Scanner(System.in);
 }
